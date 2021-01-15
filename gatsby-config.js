21ff101695797e77b66f3a3648e22ef9bf429728
GKO95 <44629526+GKO95@users.noreply.github.com>
2021-01-15 14:46:38 +0900

    Initialize Gatsby website

---
+++ gatsby-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  siteMetadata: {
+    title: "GKO95's GitHub Pages",
+  },
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-transformer-remark",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
