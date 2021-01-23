5d325dbd669814fb60dd766e240bd6ffdb9539a5
GKO95 <44629526+GKO95@users.noreply.github.com>
2021-01-23 11:12:11 +0900

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,6 @@
 import * as React from "react"
 import Layout from "../components/layout"
 
-const __category__ = ["Programming", "Library", "Computer Science"];
-
 const Docs = (props) => {
   const listGroups = props.groups.map((group, index) =>
       <li key={index}>{group}</li>
@@ -16,7 +14,6 @@ const Docs = (props) => {
 const IndexPage = () => {
   return (
     <Layout>
-      <Docs groups={__category__}/>
       <a href="/docs/en.PRGMING_Python">Python</a>
       <a href="/docs/ko.PRGMING_Python">파이썬</a>
     </Layout>
