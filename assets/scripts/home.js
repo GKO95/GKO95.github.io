07028d52840cfd32d32eff3fe0907679b07485f4
Gihawn Ko <44629526+GKO95@users.noreply.github.com>
2021-08-24 23:00:51 +0900

    Minor edits on detail stylings

--- assets/scripts/home.js
+++ assets/scripts/home.js
@@ -3,18 +3,20 @@
 //========================================
 if (config.GetLANG())
 {   // KOREAN
-    $(`#header-about`).text("소개글")
-    $(`#header-post`).text("블로그")
+    $(`#header-about`).text("소개글").attr("title", "소개글")
+    $(`#header-post`).text("블로그").attr("title", "블로그")
 
+    $(`#home-notice > a`).attr("title", `알림 | ${$(`#home-notice > a`).attr("title")}`)
     $(`.home-category`).each(function() {
         if ($(this).attr("lang") == "en") $(this).remove()
     })
 }
 else
 {   // ENGLISH
-    $(`#header-about`).text("ABOUT")
-    $(`#header-post`).text("BLOG")
+    $(`#header-about`).text("ABOUT").attr("title", "About")
+    $(`#header-post`).text("BLOG").attr("title", "Blog")
 
+    $(`#home-notice > a`).attr("title", `Notice | ${$(`#home-notice > a`).attr("title")}`)
     $(`.home-category`).each(function() {
         if ($(this).attr("lang") == "ko") $(this).remove()
     })
